76c17f77dd2771f71dbbcd2ef1abcbcd11240a58
ADITYA IMAM ZUHDI <20102217@ittelkom-pwt.ac.id>
2024-10-28 10:08:19 +0700

    add validation for CRUD task management

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsOptional } from "class-validator";
+import { IsString } from "class-validator";
+import { IsEnum } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTasksFilterDto {
+    @IsOptional()
+    @IsEnum(TaskStatus)
     status?: TaskStatus;
+    @IsOptional()
+    @IsString()
     search?: string;
 }
